23bbdcc343f75be0bcc3dc2bbbddd9a8158c35f2
NEWnumber <121753743+asadbekTemirboev@users.noreply.github.com>
2024-03-22 14:06:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$('.owl-carousel').owlCarousel({
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+
+$('.owl-carousel').owlCarousel({
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+
